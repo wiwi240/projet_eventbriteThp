0a77c3f6f8bdfa52b0e5f43c11c1e8c7d664d547
wiwi240 <william.mahi14@gmail.com>
2026-02-27 00:09:06 +0100

    fix problem roll menu

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,10 +1,9 @@
-# config/importmap.rb
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 
-# Verrouille ces deux noms pour le JS
-pin "bootstrap", to: "bootstrap.js"
-pin "@popperjs/core", to: "@popperjs/core.js"
+# SOLUTION : Liens directs pour éviter les erreurs "missing path"
+pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js"
+pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.3/dist/js/bootstrap.esm.js"
